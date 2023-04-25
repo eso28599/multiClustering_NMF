bbfcb126bd391a011eecc4e144c143e5d86e090f
eso28599 <eso18@ic.ac.uk>
2023-04-25 10:21:26 +0100

    Got sim on server working

---
+++ sim_studies_NMTF/script_test2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#  script_test2.sh
+#  
+#
+#  Created by Ella Orme on 26/03/2023.
+#  
---
+++ sim_studies_NMTF/test_script.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#  test_script.sh
+#  
+#
+#  Created by Ella Orme on 24/03/2023.
+#  
